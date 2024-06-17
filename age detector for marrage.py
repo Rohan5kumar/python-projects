3d2346baecfa4d62fb0ce2b9f65514f54f138674
Rohan <155552554+Rohan5kumar@users.noreply.github.com>
2024-06-17 11:50:28 +0530

    Add files via upload

---
+++ age detector for marrage.py
@@ -0,0 +1,9 @@
+age= int(input("enter the age : "))
+
+if age <18:
+    print('your are not eligible to marry ')
+
+elif age >60:
+    print('you are too old to marry')
+else:
+    print('we are finding best match for you')
